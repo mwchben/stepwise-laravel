6d8015b93cf0a1581e79e290584c3a7c9fcd73ec
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-02-18 11:44:20 +0300

    changes to readme and app name

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
-    <title>Document</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
+
 <body>
     @include('include.navbar')
     @include('include.br')
