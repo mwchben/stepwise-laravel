127a618fb353745f8fda0e8a822278d5d3b13cb7
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-03-16 01:18:25 +0300

    redundant code

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,8 +11,8 @@
 
 <div class="col-12" style="color: #DFA">
     <a class="btn btn-dark" href="/posts">Back</a>
-    <a class="btn btn-dark" href="/posts/editQuote/{{$aPost->id}}">Edit</a>
-    {{-- <a class="btn btn-danger" href="/posts/{{$aPost->id}}/delete-quote">Delete</a> --}}
+    <a class="btn btn-dark" href="/posts/editQuote/{{$aPost->id}}">Edit</a>    
+
     <form method="POST" action="{{ url('posts/delete/'.$aPost->id) }}">
         @method('DELETE')
         @csrf
