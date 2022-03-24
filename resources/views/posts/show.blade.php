a30509e8f88a87b6382402a829748ceac890a961
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-03-24 20:41:14 +0300

    show quote page modified

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 
 @section('content') 
+<style>
+  .list-group-item {
+    background-color: transparent;
+}
+</style>
+   
+    <div class="list-group-flush">
+        <div class="list-group-item">
+            <div class="d-flex w-100 justify-content-between">
+                <h3 class="mb-1">{{$aPost->quote}}</h3>              
+            </div>
+            <h6 class="mb-3">~{{$aPost->bywho}}</h6>
+            <p class="mb-1">{{$aPost->description}}</p>
+        </div>
+        <div class="list-group-item">
+            <div class="d-flex w-100 justify-content-between">
+                <small>by {{$aPost->user->name}}</small>
+                <small class="mb-1">{{$aPost->created_at}}</small>
+            </div>
+        </div>
+    </div>
 
-<h1>{{$aPost->quote}}</h1>
-<h4>~{{$aPost->bywho}}</h4>
-
-<div><p>{{$aPost->description}}</p></div>
-<h6>by {{$aPost->user->name}}</h6>
-<small>on{{$aPost->created_at}}</small>
 <br><br>
 
-<div class="col-12" style="color: #DFA">
-    <a class="btn btn-dark" href="/posts">Back</a>
-    <a class="btn btn-dark" href="/posts/editQuote/{{$aPost->id}}">Edit</a>    
+
+    <a href="/posts"><button type="button" class="btn btn-outline-dark">Back</button></a>
+    <a href="/posts/editQuote/{{$aPost->id}}"><button type="button" class="btn btn-outline-dark">Edit</button></a>  
 
     <form method="POST" action="{{ url('posts/delete/'.$aPost->id) }}">
         @method('DELETE')
         @csrf
-        <button type="submit" class="btn btn-danger">Remove Quote</button>
+        <button type="submit" class="btn btn-outline-dark">Remove Quote</button>
     </form>
 </div>    
 @endsection
