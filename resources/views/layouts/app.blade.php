1ffd6d62991df51944c1565e5007d7a18f43f1d3
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-03-30 22:10:03 +0300

    using cdn now😁

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,9 @@
     <title>{{ config('app.name', 'Qoutes') }}</title>
     
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Scripts (down b4 closing body tag)-->
+    {{--prefer this <script src="{{ asset('js/app.js') }}" defer></script> --}}
+    {{--done by putting the js and css folders in public folder--}}
     {{-- <script src="{{ asset('js/bootstrap.min.js') }}"></script> --}}
     
 
@@ -27,11 +28,11 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{--prefer this <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     {{-- <link href={{ asset('css/bootstrap.min.css') }} rel="stylesheet"> --}}
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{-- <link href={{ asset('css/bootstrap.min.css') }} rel="stylesheet"> --}}
-
+    
+    
 </head>
 <style>
 html,body {
@@ -56,7 +57,8 @@ html,body {
     </div>
 
     @include('include.footer')
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 </body>
 </html>
 
